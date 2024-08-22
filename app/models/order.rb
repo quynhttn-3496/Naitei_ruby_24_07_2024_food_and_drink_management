8a72f12612b1bad72db011ba9795a60d69cc52f0
Linh Dinh <dinh.thi.khanh.linh@sun-asterisk.com>
2024-08-22 16:36:37 +0700

    Search By Product

--- app/models/order.rb
+++ app/models/order.rb
@@ -12,6 +12,7 @@ class Order < ApplicationRecord
   belongs_to :address
 
   enum status: {failed: 0, succeeded: 1, confirming: 2}
+
   accepts_nested_attributes_for :address, :payment_method, :order_items
   monetize :total_invoice_cents, with_model_currency: :currency, allow_nil: true
 
