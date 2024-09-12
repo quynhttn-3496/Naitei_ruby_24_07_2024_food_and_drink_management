74d5f8aea7fc4b6ef7dbb96622213bb997ef7978
quynhttn-3496 <tran.thi.nhu.quynh@sun-asterisk.com>
2024-09-12 15:09:25 +0700

    Rspec for reviews model

---
+++ spec/factories/products.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :product do
+    name { "Sample Product" }
+    price_cents { 10000 }
+    quantity_in_stock { 10 }
+    association :category
+    currency { "USD" }
+  end
+end
